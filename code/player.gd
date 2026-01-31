5996b35b8274523f2042d3d6379f3745dbf17501
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2026-01-31 15:06:57 -0800

    added scenes

--- code/player.gd
+++ code/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
-# TODO: change speed
-const MOVE_VEL = 10
+const MOVE_VEL = 1.5 * 60
 
 func _ready() -> void:
 	pass
@@ -9,7 +8,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var horz_dir = Input.get_axis("left", "right")
 	var vert_dir = Input.get_axis("up", "down")
-	velocity.x = move_toward(velocity.x, horz_dir * MOVE_VEL, 1)
-	velocity.y = move_toward(velocity.y, vert_dir * MOVE_VEL, 1)
-
+	velocity.x = horz_dir * MOVE_VEL
+	velocity.y = vert_dir * MOVE_VEL
+	
+	# TODO: change sprite
+	if horz_dir != 0:
+		pass
+	elif vert_dir != 0:
+		pass
+		
 	move_and_slide()
