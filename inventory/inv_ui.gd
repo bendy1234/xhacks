efbab345ab8e6d60c4992139c115153fda39f785
tommy <tommyduong03@gmail.com>
2026-01-31 21:32:47 -0800

    Inventory stuff

---
+++ inventory/inv_ui.gd
@@ -0,0 +1,26 @@
+extends Control
+
+@onready var inv: Inv = preload("res://inventory/items/playerinv.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
+var is_open = false
+func update_slots():
+	for i in range(min(inv.items.size(), slots.size())):
+		slots[i].update(inv.items[i])
+func _input(event):
+	if event.is_action_pressed("open_inventory"):
+		if is_open:
+			close()
+		else:
+			open()
+			
+func _ready():
+	# update_slots()
+	close()
+	
+func open():
+	visible = true
+	is_open = true
+func close():
+	visible = false
+	is_open = false
