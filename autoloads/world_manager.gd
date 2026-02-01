14fab2021f5e6d6b5d916080f8df44c63358fc08
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2026-01-31 16:17:27 -0800

    add sceen switching

--- autoloads/world_manager.gd
+++ autoloads/world_manager.gd
@@ -1,10 +1,16 @@
 extends Node
 
+
+const mine := preload("res://scenes/mine.tscn")
+const map := preload("res://scenes/map.tscn")
+const town := preload("res://scenes/town.tscn")
+var current_lvl: Node
+
 #signal enter_mine(player, id)
 #signal enter_town(player, id)
 
 # list of tilemap data, id -> idx
-var mines = []
+var mine_data = []
 
 
 enum Area {
@@ -17,10 +23,31 @@ enum Area {
 	#enter_mine.connect(enter_mine_)
 	#enter_town.connect(enter_town_)
 
-# TODO: switch sceens
-
 func enter_mine(player: Player):
+	# TODO: check if player is in a mine already, if so, reuse the current level
+	if current_lvl != null:
+		remove_child(current_lvl)
+
 	player.area = Area.MINE
+	current_lvl = mine.instantiate();
+	add_child(current_lvl)
+	# TODO: load mine data
 	
 func enter_town(player: Player):
+	if current_lvl != null:
+		remove_child(current_lvl)
+
 	player.area = Area.TOWN
+	current_lvl = mine.instantiate();
+	add_child(current_lvl)
+	# TODO: start pos
+
+
+func enter_map(player: Player):
+	if current_lvl != null:
+		remove_child(current_lvl)
+
+	player.area = Area.MAP
+	current_lvl = map.instantiate();
+	add_child(current_lvl)
+	# TODO: start pos
--- code/main.gd
+++ code/main.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-var mine = load("res://scenes/mine.tscn")
-var map = load("res://scenes/map.tscn")
-var town = load("res://scenes/town.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
