7743c7ef712160e8f01632d21a323b6fcba9b680
bendy1234 <83143663+bendy1234@users.noreply.github.com>
2026-02-01 02:38:52 -0800

    hud stuff

---
+++ autoloads/game_stuff.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var rent_time = 5 * 60
+var time_till_rent = rent_time
+var rent_amount = 10
+
+func _physics_process(delta: float) -> void:
+	time_till_rent -= delta
+	if time_till_rent <= 0:
+		rent_time *= 1.2
+		rent_amount *= 1.5
+		time_till_rent = rent_time
+		# TODO: subtract rent from player
---
+++ code/hud.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func format_time(seconds: float) -> String:
+	var minutes = int(seconds / 60)
+	var secs = int(seconds) % 60
+	return "%02d:%02d" % [minutes, secs]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$time.text = format_time(GameStuff.time_till_rent)
